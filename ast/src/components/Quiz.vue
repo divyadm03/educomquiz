3687af0eebace2b85fd7e7c23e743736ef0eb1fb
divyadm03 <dmdiyadm03@gmail.com>
2019-09-12 15:30:38 +0530

    files

---
+++ ast/src/components/Quiz.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="quiz">
+    <h1>This is an quiz page</h1>
+     
+    <!--<div v-for-key="data in myJson"> {{data}}</div> -->
+  </div>
+</template>
+ <script>
+       import JSON from 'C:/Users/Divya/Desktop/trial/ast/src/jsonn/data1.json'
+ 
+      export default{
+          data(){
+              return{
+                  myJson: JSON 
+              }
+          }
+      }
+</script>
